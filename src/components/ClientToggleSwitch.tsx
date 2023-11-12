84bf666d1107841b16e0d8ca212a4b12d85ad576
SKT-STK <kwasny.sebastian13@gmail.com>
2023-11-12 01:09:08 +0100

    .

--- src/components/ClientToggleSwitch.tsx
+++ src/components/ClientToggleSwitch.tsx
@@ -8,6 +8,8 @@ interface ClientToggleSwitchProps {
 }
 
 const ClientToggleSwitch = ({ children, tcpPath, className }: ClientToggleSwitchProps) => <ToggleSwitch
-  clientCallback={v => window.ipcRenderer.send('tcp', {path: tcpPath, val: v === 0 ? 1 : 0})}
-  className={className}>{ children }</ToggleSwitch>
+    clientCallback={v => window.ipcRenderer.send('tcp', {path: tcpPath, val: v === 0 ? 1 : 0})}
+    className={className}>
+  { children }
+</ToggleSwitch>
 export default ClientToggleSwitch
