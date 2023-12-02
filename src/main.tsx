9b32a0ce224d54365a97b9cd98ea7dafd0e7688e
SKT-STK <kwasny.sebastian13@gmail.com>
2023-12-02 15:12:29 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '@/styles/index.css'
-import '@/styles/tailwind.css'
+import '@/styles/index.scss'
+import '@/styles/tailwind.scss'
 import { HashRouter as Router } from "react-router-dom"
 import Routes from '@/components/routes/Routes.tsx'
 
